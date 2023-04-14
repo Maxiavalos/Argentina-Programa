8a6563b09a6aacd1435d9ec64a35e198b8460be8
Santiago Girolami <santigirolami1@gmail.com>
2023-04-14 20:10:16 -0300

    agregue el modal de reservas, falta validar los campos.Muestra en consola todos los datos de la reserva

--- js/logic.js
+++ js/logic.js
@@ -29,3 +29,27 @@ listElements.forEach(listElement=>{
         listElement.classList.toggle('arrow');
     })
 })
+
+
+
+const reservarBtn = document.querySelector('#botonreserva');
+const reservarForm = document.querySelector('#reservar-form');
+const reservarclick = document.querySelector('#botondereservar');
+
+reservarclick.addEventListener('click', (event) => {
+  event.preventDefault();
+  const nameInput = document.querySelector('#name');
+  const emailInput = document.querySelector('#email');
+  const dateInput = document.querySelector('#date');
+  const timeInput = document.querySelector('#time');
+  const partySizeInput = document.querySelector('#party-size');
+  const reservation = {
+    name: nameInput.value,
+    email: emailInput.value,
+    date: dateInput.value,
+    time: timeInput.value,
+    partySize: partySizeInput.value
+  };
+  console.log(reservation); // envía la reserva a un servidor o API en lugar de imprimir en la consola
+  reservarForm.reset(); // borra los datos del formulario después de enviar
+});
